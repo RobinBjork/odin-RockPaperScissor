abc3f54600a52757ac291c8f26759644f0badd2a
RobinBjork <robin.bjorkmol@gmail.com>
2024-07-10 15:11:20 +0200

    Made random number logic work

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("h1");
+
+function getComputerChoice(value) {
+    return Math.random(value);
+    
+}
+let x = getComputerChoice();
+console.log(x);
+
+//Generate a random number between 0-1
+//if number is 0.33 or less give rock
+//If number is betwwen 0.34 and 0.66 give paper
+//If number is between 0.67 and 1 give scissor
