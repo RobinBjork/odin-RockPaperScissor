75438c59117695a03b232e9ce815e7f1a9d7eb0e
RobinBjork <robin.bjorkmol@gmail.com>
2024-07-10 15:22:24 +0200

    Made logic for rock paper scissor AI

--- index.js
+++ index.js
@@ -3,10 +3,20 @@ function getComputerChoice(value) {
     return Math.random(value);
     
 }
+//Generate a random number between 0-1
+
 let x = getComputerChoice();
+if (x <= 0.33) {
+    console.log("Rock!");
+}
+else if (x >= 0.34 && x <= 0.66) {
+    console.log("Paper!");
+}
+else {
+    console.log("Scissor!");
+}
 console.log(x);
 
-//Generate a random number between 0-1
 //if number is 0.33 or less give rock
 //If number is betwwen 0.34 and 0.66 give paper
 //If number is between 0.67 and 1 give scissor
