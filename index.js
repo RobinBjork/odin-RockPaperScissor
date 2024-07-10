140d71aa24d0f150834b596acb4d3b02ba935fa5
RobinBjork <robin.bjorkmol@gmail.com>
2024-07-10 15:57:07 +0200

    Working on human logic

--- index.js
+++ index.js
@@ -20,3 +20,20 @@ console.log(x);
 //if number is 0.33 or less give rock
 //If number is betwwen 0.34 and 0.66 give paper
 //If number is between 0.67 and 1 give scissor
+
+function getHumanChoice() {
+    let choice = window.prompt('Choose "Rock", "Paper" or "Scissor"');
+    return choice;
+}
+
+let y = getHumanChoice();
+if (y = 'Rock') {
+    console.log('hRock');
+}
+else if (y = 'Paper') {
+    console.log('hPaper');
+}
+
+//Get a value rock, paper or scissor
+//Save that value
+//Use that value to prompt the choice
